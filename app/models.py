62e5d76209621ab10f7d97e72e2980255c5f8ae6
mark <mark.getrost805@gmail.com>
2021-02-26 12:41:34 -0800

    followers implemented

--- app/models.py
+++ app/models.py
@@ -5,6 +5,15 @@ from flask_login import UserMixin
 from app import login
 from hashlib import md5
 
+
+# followers is an auxillary table that exhibits a many-to-many relationship.
+# auxillary tables in SQL terms typically store only foreign keys, which is
+# why we don't construct it as a Model subclass.
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
+
 class User(UserMixin, db.Model):
     """
     Class for our blog's users. It extends UserMixin and db.Model.
@@ -21,7 +30,49 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-    
+
+
+    '''
+    implementing followers
+    the following relationship implements this idea:
+    A follows B. A is the left User. B is the right User.
+    If we query A, we get the list of users followed by A.
+
+    Below,
+    • 'User' is the user followed by A.
+    • secondary is the aux. table above this class.
+    • primary join is the condition that links A to B.
+        - the follower id matches self.id (first var. in this class above)
+    • secondaryjoin links B to A. B is the followed user, so they have the
+      same id.
+    • backref
+
+    '''
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+
+    def followed_posts(self):
+        """
+        Query the data base for all posts from the followed users
+        of a user.
+
+        Below, we are joining the Post table with the aux. followers table.
+        From that table, we filter by which the followed user == Post.user_id.
+        Then, we want to include (union in) our own posts into the timeline.
+        So we find our posts and union them with the followed posts.
+        Finally, we order by descending order of timestamp.
+
+        """
+        just_followed_posts = Post.query.join(
+        followers, (followers.c.followed_id==Post.user_id)).filter(
+        followers.c.follower_id==self.id)
+        own_posts = Post.query.filter(user_id==self.id)
+        return just_followed_posts.union(own_posts).order_by(Post.timestamp.desc())
+
     def __repr__(self):
         """
         This dictates how a User object is printed.
@@ -43,7 +94,17 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?=identicon&s={}'.format(digest, size)
 
-
+    '''
+    Implement following and unfollowing other users.
+    '''
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+    def is_following(self, user):
+        return self.followed(filter(followers.c.followerd_id==user.id)).count() > 0
 
 @login.user_loader
 def load_user(id):
--- driver.py
+++ driver.py
@@ -1,6 +1,13 @@
 from app import app, db
-from app.models import User, Post
+from app.models import User, Post, followers
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    """
+    This function is evoked when running 'flask shell' from the shell.
+    It allows us to play with the database from the shell without
+    having to import app.models.<model>.
+    The keys in the dictionary that is returned are the strings we'll write in
+    the shell when referencing our database models.
+    """
+    return {'db': db, 'User': User, 'Post': Post, 'followers': followers}
